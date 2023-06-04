6500d9dd6aa8c7bca5df577c3af9b0c8b76166ba
Vadim <vertersssr@mail.ru>
2023-06-04 19:30:02 +0800

    статик метод

--- ооп/геттеры,сеттеры,декораторы/class3.py
+++ ооп/геттеры,сеттеры,декораторы/class3.py
@@ -1,20 +1,31 @@
 from datetime import datetime as dt
 
 class Player:
-    __LEVEL, __HEALT = 1, 100#Константы, свойтсва класса
+    __LEVEL, __HEALTH = 1, 100#Константы, свойтсва класса
     __slots__ = ['__level', '__health', '__born']
     def __init__(self):
         self.__level = Player.__LEVEL#приватные свойтсва, инкапсулированные
-        self.__health = Player.__HEALT
+        self.__health = Player.__HEALTH
         self.__born = dt.now()
     @property#деккоратор. Декораторы нужны когда идет командная разработка
     def lvl(self):#Геттер
         return self.__level
     @lvl.setter
     def lvl(self, num):#Сеттер
-        self.__level += num
+        self.__level += Player.__typeTest(num)
         if self.__level >= 100: self.__level = 100
+    @classmethod#этот декоратор нужен для работы с полями самого класса а не экземпляра(обьекта)
+    def set_cls_field(cls, lvl=1, health=100):
+        cls.__LEVEL = Player.__typeTest(lvl)
+        cls.__HEALTH = Player.__typeTest(health)
+    @staticmethod#статичный метод, работает ни с классом ни с экземпляром а просто служебный метод
+    def __typeTest(value):#приватный метод
+        if isinstance(value, int):#isinstance проверяет значение на тип
+            return value
+        else:
+            raise TypeError("Должен быть int")
 
+Player.set_cls_field(10)
 x = Player()#Создали игрока
 
 print(x.lvl)
